2cc8cc3b607a89d7cef62ed018d01a5b802b002a
GaryDMcDonald <100050366+GaryDMcDonald@users.noreply.github.com>
2022-04-25 20:44:39 -0500

    Create Nameart.py

---
+++ Nameart.py
@@ -0,0 +1,12 @@
+Name = 'Gary McDonald'
+fact = 'I am trying to change careers'
+art = '''
+ ggg     a    rrrr   y   y
+g   g   a a   r   r   y y  
+g      a   a  r  r     y
+ggggg  aaaaa  rr       y
+g   g  a   a  r r      y
+g   g  a   a  r  r     y
+ ggg   a   a  r   r    y'''
+print('My first name is ' + art)
+print(fact)
